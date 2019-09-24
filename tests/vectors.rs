c2c4b46bfba8b97789aa222c048f2950c4239be3
James McGlashan <unreachable@darkfox.id.au>
2019-09-24 00:00:00 +0000

    added AGPL headers and expanded the test vectors. 12 pass. 1024 are skipped due to lack of vector-AAD support in the C implementation

--- tests/vectors.rs
+++ tests/vectors.rs
@@ -1,47 +1,76 @@
+// tests/vectors.rs - aez test vectors
+// Copyright (C) 2019  Katzenpost Developers
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 use aez::Aez;
 
-const TEST_VECTORS: &str = include_str!("encrypt_no_ad.json");
+const TEST_VECTORS: &str = include_str!("aez.json");
 
 #[derive(serde_derive::Deserialize)]
-struct TestCase {
-    k: String,
-    nonce: String,
+struct TestCase<'a> {
+    k: &'a str,
+    nonce: &'a str,
     // unused
-    _data: Vec<String>,
+    data: Vec<&'a str>,
     // must equal c.len() - m.len()
     tau: u32,
-    m: String,
-    c: String,
+    m: &'a str,
+    c: &'a str,
 }
 
 #[test]
 fn vectors() {
-    let mut ct = Vec::new();
-    let mut pt = Vec::new();
-
     for TestCase {
-        k, nonce, tau, m, c, ..
+        k,
+        nonce,
+        data,
+        tau,
+        m,
+        c,
     } in serde_json::from_str::<Vec<TestCase>>(TEST_VECTORS).unwrap()
     {
-        eprintln!("Testing: k={}, n={}, m={}, c={}", k, nonce, m, c);
+        eprintln!("test start");
 
         let n = hex::decode(nonce).expect("nonce contains invalid hex");
         let k = hex::decode(k).expect("key contains invalid hex");
         let m = hex::decode(m).expect("message contains invalid hex");
+        let aad = data
+            .into_iter()
+            .map(|d| hex::decode(d).expect("data contains invalid hex"))
+            .collect::<Vec<Vec<u8>>>();
         let c = hex::decode(c).expect("ciphertext contains invalid hex");
         assert_eq!(tau as usize, c.len() - m.len(), "invalid test vector");
 
-        pt.clear();
-        pt.resize(m.len(), 0);
-        ct.clear();
-        ct.resize(c.len(), 0);
-
         let cipher = Aez::new(&k);
 
-        cipher.encrypt(&n, &[], &m, &mut ct);
+        let aad = match aad.len() {
+            0 => &[][..],
+            1 => &aad[0],
+            n => {
+                eprintln!("skipping AAD test with {} parts", n);
+                continue;
+            }
+        };
+
+        let mut ct = vec![0u8; c.len()];
+        let mut pt = vec![0u8; m.len()];
+
+        cipher.encrypt(&n, aad, &m, &mut ct);
         assert_eq!(ct, c);
         assert!(
-            cipher.decrypt(&n, &[], &c, &mut pt).is_ok(),
+            cipher.decrypt(&n, aad, &c, &mut pt).is_ok(),
             "decryption failed"
         );
         assert_eq!(pt, m);
